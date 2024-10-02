72bee4f83c5883769f4e07b7f30d9ebd4fd099c2
PhileasFogg3 <philip.beswick@hotmail.com>
2024-10-02 22:46:57 +0100

    Repository Initialization

---
+++ src/main/java/net/phileasfogg3/petlife/PetLife.java
@@ -0,0 +1,17 @@
+package net.phileasfogg3.petlife;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PetLife extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
